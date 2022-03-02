0480fb9f18226e39184a2d5bcfbbc37ee5d19283
Christopher Rush <ceo@christopherhustle.com>
2022-03-02 20:08:05 +0100

    styling

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,30 +1,64 @@
 import React, { useLayoutEffect } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, ScrollView, Image, StyleSheet } from 'react-native';
 
 import HeaderButtonsRight from "../components/HeaderButtonsRight";
+import DefaultText from "../components/DefaultText";
 
-const MealDetailsScreen = props => {
-    useLayoutEffect(() => {
-        props.navigation.setOptions({
-            headerRight: HeaderButtonsRight
-        })
-    }, [props.navigation]);
-    
-    const item = props.route.params.item;
-    
-    return (
-        <View style={ styles.root }>
-            <Text>The Meal Details Screen!</Text>
-        </View>
-    );
+const ListItem = props => {
+	return (
+		<View style={ styles.listItem }>
+			<DefaultText>{ props.children }</DefaultText>
+		</View>
+	)
 };
 
-const styles = StyleSheet.create( {
-    root: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-} );
+const MealDetailsScreen = props => {
+	useLayoutEffect(() => {
+		props.navigation.setOptions({
+			headerRight: HeaderButtonsRight
+		})
+	}, [ props.navigation ]);
+	
+	const item = props.route.params.item;
+	
+	return (
+		<ScrollView>
+			<Image source={ { uri: item.imageUrl } } style={ styles.image }/>
+			<View style={ styles.details }>
+				<DefaultText>{ item.duration }m</DefaultText>
+				<DefaultText>{ item.complexity.toUpperCase() }</DefaultText>
+				<DefaultText>{ item.affordability.toUpperCase() }</DefaultText>
+			</View>
+			<Text style={ styles.textTitle }>Ingredients</Text>
+			{ item.ingredients.map(ingredient => <ListItem key={ ingredient }>{ ingredient }</ListItem>) }
+			<Text style={ styles.textTitle }>Steps</Text>
+			{ item.steps.map(step => <ListItem key={ step }>{ step }</ListItem>) }
+		</ScrollView>
+	);
+};
+
+const styles = StyleSheet.create({
+	image: {
+		width: '100%',
+		height: 200
+	},
+	details: {
+		flexDirection: 'row',
+		padding: 15,
+		justifyContent: 'space-around'
+	},
+	textTitle: {
+		fontFamily: 'open-sans-bold',
+		fontSize: 20,
+		textAlign: 'center'
+	},
+	listItem: {
+		marginVertical: 5,
+		marginHorizontal: 20,
+		borderColor: '#ccc',
+		borderWidth: 1,
+		padding: 10
+	}
+});
 
 export default MealDetailsScreen;
